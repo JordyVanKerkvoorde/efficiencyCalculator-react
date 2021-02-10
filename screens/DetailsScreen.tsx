20d0db51e0dacf71543a4829b1a7a5eaf57bd734
Jordy <jordy.vankerkvoorde@student.hogent.be>
2021-02-10 23:37:53 +0100

    Add bottomtab navigation

---
+++ screens/DetailsScreen.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export default function DetailsScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details Screen</Text>
+        {/* <Button
+            title="Go to Details... again"
+            onPress={() => navigation.push('Details')}
+        />
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+        <Button title="Go back" onPress={() => navigation.goBack()} /> */}
+        </View>
+    );
+}
